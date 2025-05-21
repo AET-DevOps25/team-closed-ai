5d84abff50e9a0ab9357c1bc7eb09f1d5be7dd3a
Simon Karan <simon.karan@tum.de>
2025-05-21 12:24:51 +0200

    add server template

---
+++ server/src/main/java/de/tum/cit/aet/closed/ai/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package de.tum.cit.aet.closed.ai.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
