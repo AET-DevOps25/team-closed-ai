a30d93f46588d6a854678b9477036b749e03e8d3
Dominik Remo <47261058+DominikRemo@users.noreply.github.com>
2025-07-03 00:55:09 +0200

    Adapt pgai conf

--- genai/pgai_vec/vectorizers.py
+++ genai/pgai_vec/vectorizers.py
@@ -12,38 +12,59 @@ from pgai.vectorizer.configuration import (
 
 load_dotenv()
 
-__embedding_config = EmbeddingOllamaConfig(
+_embedding_config = EmbeddingOllamaConfig(
     model=os.environ["EMBED_MODEL"],
     dimensions=int(os.environ["EMBED_DIMENSIONS"]),
 )
 
-__task_vectorizer = CreateVectorizer(
+_task_template = """
+Task Entry: {
+ Title: $chunk,
+ Status: $status,
+ User ID: $assignee_id,
+ Description: $description
+}
+"""
+
+
+_task_vectorizer = CreateVectorizer(
     source="task",
     name="task_vectorizer",
     destination=DestinationTableConfig(destination="task_embedding"),
     loading=LoadingColumnConfig(column_name="title"),
-    embedding=__embedding_config,
+    embedding=_embedding_config,
     chunking=ChunkingNoneConfig(),
-    formatting=FormattingPythonTemplateConfig(
-        template="Title: $chunk\nStatus: $status\n\nDescription: $description"
-    ),
+    formatting=FormattingPythonTemplateConfig(template=_task_template),
     enqueue_existing=True,
 )
 
-__project_vectorizer = CreateVectorizer(
+_project_template = """
+Project Entry: {
+ Project Name: $chunk
+}
+"""
+
+_project_vectorizer = CreateVectorizer(
     source="project",
     name="project_vectorizer",
     destination=DestinationTableConfig(destination="project_embedding"),
     loading=LoadingColumnConfig(column_name="name"),
-    embedding=__embedding_config,
+    embedding=_embedding_config,
     chunking=ChunkingNoneConfig(),
-    formatting=FormattingPythonTemplateConfig(template="Project Name: $chunk"),
+    formatting=FormattingPythonTemplateConfig(template=_project_template),
     enqueue_existing=True,
 )
 
 
-def get_vectorizers():
+def get_vectorizers() -> list[CreateVectorizer]:
     """
     Returns a list of vectorizers.
     """
-    return [__task_vectorizer, __project_vectorizer]
+    return [_task_vectorizer, _project_vectorizer]
+
+
+def get_views() -> dict[str, str]:
+    """
+    Returns a list of views.
+    """
+    return {_task_vectorizer.source: _task_view}
