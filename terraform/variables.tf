df5fca038dc96f9abbb89c6e407680891c1e2002
Dominik Remo <47261058+DominikRemo@users.noreply.github.com>
2025-07-16 12:37:20 +0200

    Add terraform + ansible configs for aws deployment

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "key_name" {
+  type    = string
+  default = "closedai-deployer"
+}
+
+variable "public_key_path" {
+  type    = string
+  default = "~/.ssh/closedai-deployer.pub"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.medium"
+}
+
+variable "root_size" {
+  type    = number
+  default = 24
+}
